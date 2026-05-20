3ca23990a15d720aff2dc88bfed4d1f443a5f842
AHILAN <ahilan15678@gmail.com>
2026-05-20 12:08:16 +0530

    a basic client-server model

--- client.c
+++ client.c
@@ -1,1 +1,60 @@
-	// It will include the client side code
+					// It wil include  the client  side code 
+					//argv[0] filename
+					//argv[1] server_ipaddr
+					//argv[2] port_no
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> //hostent struct
+#include <unistd.h> //read,write,close
+
+void error(const char *msg){perror(msg);exit(1);} //for system errors only, not for user side errors, bindor accept or socket(){these are system functions} generally fails and hence we use this. 
+
+int main(int argc , char *argv[]){
+	int sockfd, portno, n;
+	struct sockaddr_in server_addr;
+	struct hostent *server;
+
+	char buffer[255];
+	if(argc<3){fprintf(stderr,"usage %s hostname port",argv[0]); exit(1);}
+
+	portno = atoi(argv[2]);
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd<0){error("failed to create a socket \n");}
+
+	server = gethostbyname(argv[1]);
+	if(server == NULL){fprintf(stderr, "no such hosts \n");}
+
+	bzero((char *)&server_addr, sizeof(server_addr));	
+	server_addr.sin_family = AF_INET;
+	memcpy(&server_addr.sin_addr.s_addr,
+       server->h_addr_list[0],
+       server->h_length);
+	server_addr.sin_port = htons(portno);
+
+	if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr))<0){error("failed to connect \n");}
+
+	while(1){
+
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+		n = write(sockfd, buffer, strlen(buffer));
+		if(n<0){error("failed to write \n");}
+
+		bzero(buffer, 255);
+		n = read(sockfd, buffer, 255);
+		if(n<0){error("failed to read \n");}
+
+		printf("SERVER : %s", buffer);
+
+		int i = strncmp("bye", buffer, 3);
+		if(i == 0){break;}
+	}
+
+	close(sockfd);
+	return 0;
+}
--- server.c
+++ server.c
@@ -1,1 +1,68 @@
-			// It wil include  the server  side code 
+					// It wil include  the server  side code 
+					// argv[0] : filename
+					// argv[1] : port_no
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+
+void error(const char *msg){perror(msg);exit(1);} //for system errors only, not for user side errors, bindor accept or socket(){these are system functions} generally fails and hence we use this. 
+
+int main(int argc , char *argv[]){
+
+	if(argc<2){fprintf(stderr,"port not provided program terminated. \n");exit(1);}
+
+	int sockfd, newsockfd, portno, n;
+	char buffer[255];
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t client_len; //we need client as server would CONNECT, so WHO is connecting becomes important
+
+	//creating a socket.
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(sockfd < 0){error("failure to open socket. \n");}
+
+	//pointer,size
+	bzero((char *)&server_addr, sizeof(server_addr)); //as the serv_addr may initially be filled with random form the old stack bzero would initialize it to 0; as sometimes while i initialize after the random garbage values causes error
+
+	portno = atoi(argv[1]);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr  = INADDR_ANY;
+	server_addr.sin_port = htons(portno);
+
+
+	if(bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){error("binding failed \n");}
+	
+	listen(sockfd, 5);
+
+	client_len = sizeof(client_addr);
+
+	newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len); // creating the new socket that establishing a communicationn between the client and the server, the earliler socket was just to listen.
+
+	if(newsockfd<0){error("Connection failed \n");}
+
+	while(1){
+
+		bzero(buffer, 255);
+		n = read(newsockfd, buffer, 255);
+		if(n<0){error("reading failed \n");}
+
+		printf("Client : %s \n", buffer);
+
+		bzero(buffer,255); //for clearing the buffer 
+		fgets(buffer, 255, stdin); //takes input from the server 
+
+		n = write(newsockfd, buffer, strlen(buffer));// and this would return to client
+		if(n<0){error("failed to write \n");}
+
+		int i = strncmp("bye", buffer, 3);
+		if(i == 0){break;}
+	}
+	close(newsockfd);
+	close(sockfd);
+return 0;
+}
